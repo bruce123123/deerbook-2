6139ea3368c034b6b6e593a3ab054eb522c5cc1e
bruce-h <bdhay@protonmail.com>
2021-02-12 09:48:05 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
+
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    user: {
+      name: "Bruce Hay",
+      username: "bhay",
+      image: "",
+      firendList: []
+    },
+    apiDataLoaded: false,
+    potentialFriends: []
+  }
+}
+
+
+  Render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bruce Book</h1>
+
+
+
+
     </div>
   );
-}
+  }
+ }
 
 export default App;
