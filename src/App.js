3dfafb2900c6d57752b375384b9f057edf5c4f5a
bruce-h <bdhay@protonmail.com>
2021-02-12 13:48:17 -0600

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import Profile from './components/Profile';
 import Friends from './components/Friends';
-
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,34 +14,34 @@ class App extends Component {
         image: "https://pyxis.nymag.com/v1/imgs/5e4/dfd/c59573793cc957a78d232f82d3832af173-17-thor.rsquare.w700.jpg",
         friendList: ["Leo"]
       },
-      apiDataLoaded: true,
+      apiDataLoaded: false,
       potentialFriends: []
-    },
-    this.componentDidMount = async () => {
-      const friend1 = await axios.get("https//randomuser.me/api/");
-      const friend2 = await axios.get("https//randomuser.me/api/");
-      const friend3 = await axios.get("https//randomuser.me/api/");
-      const friend4 = await axios.get("https//randomuser.me/api/");
-      const friend5 = await axios.get("https//randomuser.me/api/");
-      console.log(friend.data.results[0]);
-      const friends= [
-        friend1.data.results[0],
-        friend2.data.results[0],
-        friend3.data.results[0],
-        friend4.data.results[0],
-        friend5.data.results[0],
-      ]
-      this.setState({
-        potentialFriends: friends
-      })
     }
+  }
+  componentDidMount = async () => {
+    const friendOne = await axios.get("https://randomuser.me/api/");
+    const friendTwo = await axios.get("https://randomuser.me/api/");
+    const friendThree = await axios.get("https://randomuser.me/api/");
+    const friendFour = await axios.get("https://randomuser.me/api/");
+    const friendFive = await axios.get("https://randomuser.me/api/");
+    const friends = [
+      friendOne.data.results[0],
+      friendTwo.data.results[0],
+      friendThree.data.results[0],
+      friendFour.data.results[0],
+      friendFive.data.results[0]
+    ];
+    this.setState({
+      potentialFriends: friends,
+      apiDataLoaded: true
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>CaseyBook</h1>
         <Profile user={this.state.user} />
-        <Friends potentialFriends={this.state.potentialFriends}/>
+        <Friends potentialFriends={this.state.potentialFriends} />
       </div>
     );
   }
